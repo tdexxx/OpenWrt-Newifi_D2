5eda06a1cc29b81dec2c01709a2c896c3f8e85c8
gooduc <gooduc@gmail.com>
2021-08-17 20:31:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,9 @@
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+
+#sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
+
+
+
+
